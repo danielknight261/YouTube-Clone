60e3eb61a32e03a650882cad1265c05949b5f840
Dan Knight <danielknight261@gmail.com>
2024-04-24 10:30:05 +0100

    small side bar

--- vite-project/src/layouts/Sidebar.tsx
+++ vite-project/src/layouts/Sidebar.tsx
@@ -1,9 +1,26 @@
+import { Home } from "lucide-react"
+import { ElementType } from "react"
 
 export  function Sidebar() {
-
   return (
   <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
-   evfvfds
+   <SidebarItem  Icon={Home} title="Home" url="/" />
   </aside>
 )
 }
+
+type SidebarItemProps = { 
+Icon: ElementType
+title: string
+url: string
+}
+ 
+function SidebarItem({ Icon, title, url}:
+SidebarItemProps) {
+  return (
+  <a href={url} className="">
+    <Icon className="w-6 h-6" />
+    <div className="text-sm"> {title}</div>
+    </a>
+  )
+  }
